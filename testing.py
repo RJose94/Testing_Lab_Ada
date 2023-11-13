f73ab78ec753e5860234020a487797469ec55547
RJose94 <137239149+RJose94@users.noreply.github.com>
2023-11-12 19:12:47 -0500

    Testing_Lab

---
+++ testing.py
@@ -0,0 +1,26 @@
+import unittest
+from datetime import datetime
+
+class Greeter:
+    def greet(self, name):
+        name = name.strip().capitalize()
+        hour = datetime.now().hour
+
+        if 6 <= hour < 12:
+            greeting = 'Buenos dias'
+        elif 18 <= hour < 22:
+            greeting = 'Bunas tardes'
+        else:
+            greeting = 'Buenas noches'
+
+        print(f'{greeting} {name}')
+        return f'{greeting} {name}'
+
+
+class TestGreeter(unittest.TestCase):
+    def test_greet(self):
+        greeter = Greeter()
+        self.assertEqual(greeter.greet(' Jose '), 'Hola Jose')
+
+if __name__ == '__main__':
+    unittest.main()
